43f729f3f6f11cb96a08ccffb395be11596263f7
Rui Nascimento <rri.nascimento@gmail.com>
2026-03-18 23:29:03 +0000

    Improved connection

--- url-shortener/src/test/java/com/urlshortener/UrlServiceTest.java
+++ url-shortener/src/test/java/com/urlshortener/UrlServiceTest.java
@@ -14,19 +14,14 @@ import com.urlshortener.domain.Url;
 import com.urlshortener.repository.UrlRepository;
 import com.urlshortener.service.UrlService;
 
-
+// NOTE: This testing is for the services, not actually making calls to the database
 class UrlServiceTest {
     @Test
     void shouldCreateShortUrl() throws Exception {
-
         UrlRepository repo = mock(UrlRepository.class);
-
         when(repo.existsByOriginalUrl("https://google.com")).thenReturn(false);
-
         UrlService service = new UrlService(repo);
-
         String code = service.createShortUrl("https://google.com");
-
         assertNotNull(code);
         verify(repo).save(any(Url.class));
     }
@@ -34,41 +29,28 @@ class UrlServiceTest {
     @Test
     void shouldReturnNullIfUrlExists() throws Exception {
         UrlRepository repo = mock(UrlRepository.class);
-
         when(repo.existsByOriginalUrl("https://google.com")).thenReturn(true);
-
         UrlService service = new UrlService(repo);
-
         String result = service.createShortUrl("https://google.com");
-
         assertNull(result);
     }
 
     @Test
     void shouldReturnOriginalUrl() throws Exception {
         UrlRepository repo = mock(UrlRepository.class);
-
         Url url = new Url("abc123", "https://google.com");
-
         when(repo.findByShortCode("abc123")).thenReturn(url);
-
         UrlService service = new UrlService(repo);
-
         String result = service.getOriginalUrl("abc123");
-
         assertEquals("https://google.com", result);
     }
 
     @Test
     void shouldReturnNullIfCodeNotFound() throws Exception {
         UrlRepository repo = mock(UrlRepository.class);
-
         when(repo.findByShortCode("abc123")).thenReturn(null);
-
         UrlService service = new UrlService(repo);
-
         String result = service.getOriginalUrl("abc123");
-
         assertNull(result);
     }
 }
